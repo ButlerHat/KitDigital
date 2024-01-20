a3c12197b0476b63967a0fc87b9b2d9b07553b10
DavidCorreas <davidestu77@gmail.com>
2024-01-20 20:19:28 +0100

    Change address

--- .devcontainer/prod/postCreateCommand.sh
+++ .devcontainer/prod/postCreateCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd web
-/opt/conda/envs/web_env/bin/python /opt/conda/envs/web_env/bin/streamlit run app.py --server.address 127.0.0.1 --server.port 8501 --server.headless True
+/opt/conda/envs/web_env/bin/python /opt/conda/envs/web_env/bin/streamlit run app.py --server.port 8501 --server.headless True
